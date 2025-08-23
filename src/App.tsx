72b251b6721697edb0f53205ec21aa1fb56a80a5
Kawdoor <nicolas.crespo141429@alumnos.info.unlp.edu.ar>
2025-08-23 19:42:44 -0300

    testfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AlertTriangle } from 'lucide-react';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { ToastProvider } from './components/toast/Toast';
